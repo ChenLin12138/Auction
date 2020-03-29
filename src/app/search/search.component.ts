0950b78b595ee09290b66009f8e772d7b405e69b
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-29 14:38:17 +0800

    使用表单提交信息

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-search',
@@ -8,14 +9,34 @@ import { FormGroup, FormControl} from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  categories : string[];
+  formModel : FormGroup;
+
+  constructor(private productService : ProductService) { 
+    this.categories = this.productService.getAllCategories();
+    const fb = new FormBuilder();
+    //验证输入内容
+    //title至少有3个字符
+    //price需要是正数
+    this.formModel = fb.group({
+      'title' : [null, Validators.minLength(3)],
+      'price' : [null, positiveNumberValidator],
+      'category' : [-1]
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  formModel : FormGroup = new FormGroup ({
-    'title' : new FormControl(),
-    'price' : new FormControl(),
-    'category' : new FormControl()
-  });
+  onSearch(){
+    if(this.formModel.valid){
+      console.log(this.formModel.value);
+    }
+  }
+}
+
+function positiveNumberValidator(control : FormControl) : any {
+  if(!control.value ) return null;
+  const price = parseInt(control.value);
+  return price === null || typeof price === 'number' && price > 0 ? null : {positivenumber : true}
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -24,4 +24,8 @@ export class ProductService {
     .map(r => new Review (r.id, r.productId, new Date(r.timestamp), r.user, r.rating, r.comment));
   }
 
+  getAllCategories() : string[] {
+    return ['Books', 'Electronics', 'Hardware'];
+  }
+
 }
